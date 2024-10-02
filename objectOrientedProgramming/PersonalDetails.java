94c5ff94299c2d035643cc72227ac1ddc945b1bf
ybtuti <samuelmsuu@gmail.com>
2024-10-02 22:53:33 +0300

    feat: Add InvoiceOutput class to display invoice details

--- objectOrientedProgramming/PersonalDetails.java
+++ objectOrientedProgramming/PersonalDetails.java
@@ -1,12 +1,12 @@
 public class PersonalDetails {
     public static void main(String[] args) {
-        // Replace these variables with your actual details
+        
         String registrationNumber = "12345";
         String fullName = "John Doe";
         int year = 2;
         int semester = 2;
 
-        // Print each detail on its own line
+        // Prints each detail on its own line
         System.out.println("Your registration Number: " + registrationNumber);
         System.out.println("Your Full Names: " + fullName);
         System.out.println("Your Year: " + year);
